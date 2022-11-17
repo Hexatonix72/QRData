9ed5a704e06220bf450940e385c6a5918081ad59
Hexatonix72 <brk05794@sjfc.edu>
2022-11-17 17:10:44 -0500

    Update qrTest.py
    rename variables

--- qrTest.py
+++ qrTest.py
@@ -6,26 +6,27 @@ import qrcode
 
 
 class QRData:
-    def __init__(self, StartTime, DeviceID, EndTime):
-        self.StartTime = StartTime
-        self.DeviceID = DeviceID
-        self.EndTime = EndTime
+    def __init__(self, starttime, deviceid, endtime):
+        self.starttime = starttime
+        self.deviceid = deviceid
+        self.endtime = endtime
 
 
 def to_json(obj):
     return json.dumps(obj, default=lambda obj: obj.__dict__)
 
-def generateQRcode():
-    StartTimeInt = datetime.datetime.now()
-    TwentyInt = datetime.timedelta(seconds=20)
-    EndTimeInt = (StartTimeInt + TwentyInt)
-    start_time = str(StartTimeInt)
-    end_time = str(EndTimeInt)
 
-    QR1 = QRData(start_time, 0, end_time)
+def generateqrcode():
+    starttime = datetime.datetime.now()
+    twentysec = datetime.timedelta(seconds=20)
+    endtime = (starttime + twentysec)
+    start_time = str(starttime)
+    end_time = str(endtime)
 
-    QR = to_json(QR1)
-    img = qrcode.make(QR)
+    qr1 = QRData(start_time, 0, end_time)
+
+    qr = to_json(qr1)
+    img = qrcode.make(qr)
     type(img)  # qrcode.image.pil.PilImage
     img.save("QRCode.png")
 
@@ -42,10 +43,10 @@ white = (255, 255, 255)
 clock = pygame.time.Clock()
 crashed = False 
 
-generateQRcode()
+generateqrcode()
 
-TwentyInt = datetime.timedelta(seconds=20)
-EndTimeInt = (datetime.datetime.now() + TwentyInt)
+twentysec = datetime.timedelta(seconds=20)
+endtime = (datetime.datetime.now() + twentysec)
 
 
 while not crashed:
@@ -58,9 +59,9 @@ while not crashed:
 
     a = datetime.datetime.now()
     
-    if EndTimeInt < a:
-        generateQRcode()
-        EndTimeInt = (a + TwentyInt)
+    if endtime < a:
+        generateqrcode()
+        endtime = (a + twentysec)
     
     qrImg = pygame.image.load('QRCode.png')
     r = qrImg.get_rect()
